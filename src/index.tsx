24f2647e5be94da44c6de6b61169c37985488adf
JanGospodarek <jan.gospo@onet.pl>
2022-10-19 17:44:38 +0200

    final com

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import App from "./App";
-import store from "./store/store";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
+import store from "./store/store";
+import App from "./App";
+const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(
   <Provider store={store}>
     <App />
